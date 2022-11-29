6c9a4ad0e01368cc60b9a26554a10fd413448a6a
Thomas Schrøder <295184@VIAUC.DK>
2022-11-29 10:16:41 +0100

    ClassLibrary

---
+++ ClassLibraryProducts/Model/Paint.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StarkRecycleBlazorApp.Model
+{
+    public class Paint
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [MaxLength(50)]
+        public string Name { get; set; }
+        
+       
+        public string Color { get; set; }
+
+        // Mål i liter
+        [Range(0.5, 10)]
+        public double Weigth { get; set; }
+
+        public string Condition { get; set; }
+
+        public List<Paint> Paints { get; set; }
+    }
+}
---
+++ ClassLibraryProducts/Model/Tree.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StarkRecycleBlazorApp.Model
+{
+    public class Tree
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Range(1, 100)]
+        public int Amount { get; set; }
+        
+        // Mål i meter
+        [Range(0.015, 0.1)]
+        public double Height { get; set; }
+
+        // Mål i meter
+
+        [Range(0.1, 1.5)]
+        public double Width { get; set; }
+
+        // Mål i meter
+        [Range(1, 5)]
+        public int Length { get; set; }
+
+        public string Condition { get; set; }
+        public List<Tree> Trees { get; set; }
+    }
+}
