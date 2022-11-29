4b4d1c9197a5035e982c9091def7edbb7971fea2
Charlie Phat Manh Ngo <295163@VIAUC.DK>
2022-11-29 11:01:14 +0100

    preliminary Calculator

---
+++ StarkRecycleBlazorApp/Model/Paint.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StarkRecycleBlazorApp.Model
+{
+    public class Paint
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [MaxLength(50)]
+        public string Name { get; set; }
+        
+       
+        public string Color { get; set; }
+
+        // Mål i liter
+        [Range(0.5, 10)]
+        public double Weight { get; set; }
+
+        public string Condition { get; set; }
+        [Range(1,20)]
+        public int Amount { get; set; }
+
+        public List<Paint> Paints { get; set; }
+    }
+}
