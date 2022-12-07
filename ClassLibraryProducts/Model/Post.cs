8f1df1b6bc4754dc5d60c8a7185ff4f7b23a069a
Charlie Phat Manh Ngo <295163@VIAUC.DK>
2022-12-07 12:52:06 +0100

    Post kriterier ændret

--- ClassLibraryProducts/Model/Post.cs
+++ ClassLibraryProducts/Model/Post.cs
@@ -10,17 +10,19 @@ namespace ClassLibraryProducts
         public int PostID { get; set; }
 
         //public DateFormat PostDate { get; set; }
+        [StringLength(12, MinimumLength = 1, ErrorMessage = "Must include a Date between 1 - 12 Characters")]
         public string PostDate { get; set; }
 
-        [StringLength(10000, MinimumLength = 1, ErrorMessage = "Must include a title between 1 - 10000 Characters")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Must include a Description between 1 - 50 Characters")]
         public string Title { get; set; }
-        [StringLength(50, MinimumLength =1,ErrorMessage ="Must include a Description between 1 - 50 Characters")]
+
+        [StringLength(10000, MinimumLength = 1, ErrorMessage = "Must include a title between 1 - 10000 Characters")]
         public string Description { get; set; }
+
         [StringLength(20, MinimumLength = 1, ErrorMessage = "Must include a Firstname between 1 - 20 Characters")]
         public string AuthorFirstname { get; set; }
+
         [StringLength(20, MinimumLength = 1, ErrorMessage = "Must include a Lastname between 1 - 20 Characters")]
         public string AuthorLastname { get; set;}
-
-
     }
 }
