a38d4670a0b98fdf1ed4a247e542de637f12c80b
Charlie Phat Manh Ngo <295163@VIAUC.DK>
2022-12-07 14:02:19 +0100

    Ændret Range Data annotation og calculator

--- ClassLibraryProducts/Model/Paint.cs
+++ ClassLibraryProducts/Model/Paint.cs
@@ -13,11 +13,11 @@ namespace ClassLibraryProducts
         public string Color { get; set; }
 
         // Mål i liter
-        [Range(0.5, 10)]
+        [Range(0.5, 10, ErrorMessage = "Skal have en vægt på mindst 0.5Liter og maks 10 liter")]
         public double Weight { get; set; }
 
         public string Condition { get; set; }
-        [Range(1,20)]
+        [Range(1,100, ErrorMessage = "Skal have et antal på mindst 1 og maks 100 styk")]
         public int Amount { get; set; }
 
     }
--- ClassLibraryProducts/Model/Tree.cs
+++ ClassLibraryProducts/Model/Tree.cs
@@ -10,20 +10,20 @@ namespace ClassLibraryProducts
         [MaxLength(50)]
         public string TreeName { get; set; }
 
-        [Range(1, 100)]
+        [Range(1, 100,ErrorMessage = "Skal have et antal på mindst 1 og maks 100styk")]
         public int Amount { get; set; }
         
         // Mål i centimeter
-        [Range(5, 15)]
+        [Range(5, 30, ErrorMessage ="Skal have en højde på mindst 5 og maks 30cm")]
         public double Height { get; set; }
 
         // Mål i centimeter
 
-        [Range(5, 30)]
+        [Range(5, 30, ErrorMessage = "Skal have en Bredde på mindst 5 og maks 30cm")]
         public double Width { get; set; }
 
         // Mål i centimeter
-        [Range(20, 500)]
+        [Range(20, 500, ErrorMessage = "Skal have en Længde på mindst 20 og maks 500cm")]
         public double Length { get; set; }
 
         public string Condition { get; set; }
